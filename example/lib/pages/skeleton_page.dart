7f32d02ccffb493bc0ff7d2521bddcaea2b805e9
Rannie <18622218653@163.com>
2019-08-07 23:36:58 +0800

    feat: skeleton support stretch animation type

--- example/lib/pages/skeleton_page.dart
+++ example/lib/pages/skeleton_page.dart
@@ -9,6 +9,18 @@ class SkeletonPage extends StatefulWidget {
 }
 
 class _SkeletonState extends State<SkeletonPage> {
+  bool _active = true;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,11 +28,45 @@ class _SkeletonState extends State<SkeletonPage> {
         title: Text('Skeleton Page'),
       ),
       body: Container(
-        child: FLSkeleton(
-          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
-          borderRadius: BorderRadius.all(Radius.circular(4)),
-          height: 30,
-        ),
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: Stack(
+            children: <Widget>[
+              FLSkeleton(
+                shape: BoxShape.circle,
+                margin: EdgeInsets.only(top: 10, left: 10),
+                active: _active,
+                width: 40,
+                height: 40,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                margin: EdgeInsets.only(left: 60, top: 10, right: 10),
+                active: _active,
+                height: 20,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                type: FLSkeletonAnimationType.stretch,
+                margin: EdgeInsets.only(left: 60, top: 40),
+                width: 300,
+                stretchWidth: 150,
+                height: 20,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                type: FLSkeletonAnimationType.stretch,
+                margin: EdgeInsets.only(left: 60, top: 70, bottom: 10),
+                width: 100,
+                stretchWidth: 200,
+                height: 20,
+              ),
+            ],
+          ),
+        )
       ),
     );
   }
--- lib/widgets/skeleton.dart
+++ lib/widgets/skeleton.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 
-const int _kDefaultMilliSeconds = 1400;
+const int _kDefaultMS = 1000;
+
+enum FLSkeletonAnimationType {
+  shimmer,
+  stretch
+}
 
 class FLSkeleton extends StatefulWidget {
   FLSkeleton({
@@ -10,10 +15,12 @@ class FLSkeleton extends StatefulWidget {
     this.color,
     this.width,
     this.height,
-    this.constraints,
     this.margin,
     this.borderRadius,
     this.duration,
+    this.active = true,
+    this.type = FLSkeletonAnimationType.shimmer,
+    this.stretchWidth
   }) : super (key: key);
 
   final BoxShape shape;
@@ -21,10 +28,19 @@ class FLSkeleton extends StatefulWidget {
   final Color color;
   final double width;
   final double height;
-  final BoxConstraints constraints;
   final EdgeInsetsGeometry margin;
   final BorderRadiusGeometry borderRadius;
   final Duration duration;
+
+  /// Show animation or not, default is true.
+  final bool active;
+
+  /// The animation type of the skeleton, default is [FLSkeletonAnimationType.shimmer].
+  /// If you specify [FLSkeletonAnimationType.stretch] type, you must set [width] and [stretchWidth].
+  final FLSkeletonAnimationType type;
+
+  /// The 'animate to' width when choose [FLSkeletonAnimationType.stretch] type.
+  final double stretchWidth;
 
   @override
   State<FLSkeleton> createState() => _FLSkeletonState();
@@ -37,25 +53,66 @@ class _FLSkeletonState extends State<FLSkeleton> with SingleTickerProviderStateM
   @override
   void initState() {
     super.initState();
-    final Duration duration = widget.duration
-        ?? const Duration(milliseconds: _kDefaultMilliSeconds);
+    final Duration duration = widget.duration ?? const Duration(milliseconds: _kDefaultMS);
     _controller = AnimationController(
-      vsync: this,
-      duration: duration
+        vsync: this,
+        duration: duration
     );
+    _setupAnimationAndStart();
+  }
 
-    _animation = Tween<double>(begin: -1.0, end: 2.0)
-        .animate(CurvedAnimation(curve: Curves.linear, parent: _controller))
-      ..addStatusListener((status) {
-        if (status == AnimationStatus.completed ||
-            status == AnimationStatus.dismissed) {
-          _controller.repeat();
-        } else if (status == AnimationStatus.dismissed) {
-          _controller.forward();
-        }
-      });
+  @override
+  void didUpdateWidget(FLSkeleton oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    _setupAnimationAndStart();
+  }
 
-    _controller.forward();
+  void _setupAnimationAndStart() {
+    _animation = _genTween().animate(
+        CurvedAnimation(curve: Curves.linear, parent: _controller));
+
+    if (widget.type == FLSkeletonAnimationType.shimmer)
+      _animation.addStatusListener(_handleShimmerAnimationStatus);
+    else
+      _animation.addStatusListener(_handleStretchAnimationStatus);
+
+    _controllerForward();
+  }
+
+  Tween<double> _genTween() {
+    return (widget.type == FLSkeletonAnimationType.shimmer)
+        ? Tween<double>(begin: -1.0, end: 2.0)
+        : Tween<double>(begin: widget.width, end: widget.stretchWidth);
+  }
+
+  void _handleShimmerAnimationStatus(AnimationStatus status) {
+    if (status == AnimationStatus.completed ||
+        status == AnimationStatus.dismissed) {
+      _controllerRepeat();
+    }
+  }
+
+  void _handleStretchAnimationStatus(AnimationStatus status) {
+    if (status == AnimationStatus.completed) {
+      _controllerReverse();
+    } else if (status == AnimationStatus.dismissed) {
+      _controllerForward();
+    }
+  }
+
+  void _controllerReverse() {
+    if (widget.active)
+      _controller.reverse();
+  }
+
+  void _controllerRepeat() {
+    if (widget.active)
+      _controller.repeat();
+  }
+
+  void _controllerForward() {
+    if (widget.active)
+      _controller.forward();
   }
 
   @override
@@ -66,38 +123,44 @@ class _FLSkeletonState extends State<FLSkeleton> with SingleTickerProviderStateM
 
   @override
   Widget build(BuildContext context) {
-    final BoxShape shape = widget.shape ??  BoxShape.rectangle;
-    final Color color = widget.color ?? const Color(0xFFE8E8E8);
-
     return AnimatedBuilder(
-      animation: _animation,
-      builder: (context, child) {
-        return Container(
-          decoration: BoxDecoration(
-              shape: shape,
-              borderRadius: widget.borderRadius,
-              gradient: LinearGradient(
-                  begin: Alignment.centerLeft,
-                  end: Alignment.centerRight,
-                  colors: [
-                    color,
-                    color.withAlpha(80),
-                    color
-                  ],
-                  stops: [
-                    _animation.value - 0.4,
-                    _animation.value,
-                    _animation.value + 0.4
-                  ]
-              )
-          ),
-          padding: widget.padding,
-          width: widget.width,
-          height: widget.height,
-          constraints: widget.constraints,
-          margin: widget.margin,
-        );
-      }
+        animation: _animation,
+        builder: (context, child) {
+          /// config style
+          final BoxShape shape = widget.shape ??  BoxShape.rectangle;
+          final Color color = widget.color ?? const Color(0xFFE8E8E8);
+          final Gradient gradient = widget.type == FLSkeletonAnimationType.shimmer ? LinearGradient(
+              begin: Alignment.centerLeft,
+              end: Alignment.centerRight,
+              colors: [
+                color,
+                color.withAlpha(80),
+                color
+              ],
+              stops: [
+                _animation.value - 0.4,
+                _animation.value,
+                _animation.value + 0.4
+              ]
+          ) : null;
+          final double width = widget.type == FLSkeletonAnimationType.shimmer
+              ? widget.width : _animation.value;
+          final decColor = widget.type == FLSkeletonAnimationType.stretch
+              ? color : null;
+
+          return Container(
+            decoration: BoxDecoration(
+                shape: shape,
+                borderRadius: widget.borderRadius,
+                color: decColor,
+                gradient: gradient
+            ),
+            padding: widget.padding,
+            width: width,
+            height: widget.height,
+            margin: widget.margin,
+          );
+        }
     );
   }
 }
