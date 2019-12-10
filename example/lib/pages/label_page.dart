e30d62a597f95a2be7d805f2ac3a8c61a018c310
Rannie <18622218653@163.com>
2019-12-10 14:17:36 +0800

    fix: marquee label support not loop mode and finish label demo

--- example/lib/pages/label_page.dart
+++ example/lib/pages/label_page.dart
@@ -1,5 +1,7 @@
-import 'package:example/marquee_label.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:example/copyable_label.dart';
+import 'package:example/marquee_label.dart';
 
 class LabelPage extends StatefulWidget {
   static const String routeName = '/label';
@@ -9,18 +11,114 @@ class LabelPage extends StatefulWidget {
 }
 
 class _LabelPageState extends State<LabelPage> {
+  String _clipboardStr = '';
+
+  Widget _buildSection(String title, Widget content) {
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Color.fromRGBO(246, 246, 246, 1),
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: 56,
+          child: Row(
+            children: <Widget>[
+              Text(title, style: TextStyle(color: Colors.blueGrey, fontSize: 17)),
+            ],
+          ),
+        ),
+        content
+      ],
+    );
+  }
+
+  Widget _buildCopyLabelContent() {
+    String copyableStr = 'This is a copyable label';
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 10),
+        FLCopyableLabel(
+          copyData: copyableStr,
+          showMenu: true,
+          child: Padding(
+            padding: const EdgeInsets.all(5),
+            child: Text(copyableStr, style: TextStyle(fontSize: 17)),
+          ),
+          afterCopyCallback: _showPasteBoardValue,
+        ),
+        SizedBox(height: 10),
+        Padding(
+          padding: EdgeInsets.all(5),
+          child: Text('Current clipboard text is \n $_clipboardStr', style: TextStyle(fontSize: 15)),
+        ),
+        SizedBox(height: 10),
+      ],
+    );
+  }
+
+  Widget _buildMarqueeLabelContent() {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 10),
+          FLMarqueeLabel(
+            text: 'Notice: For Flutter 1.9 or above, you can use SelectableText to support copy function',
+            style: TextStyle(
+                color: Colors.redAccent,
+                fontSize: 16
+            ),
+          ),
+          SizedBox(height: 10),
+          FLMarqueeLabel(
+            text: 'Notice: FLUI is a widget kit for Google Flutter',
+            style: TextStyle(
+                color: Colors.blueAccent,
+                fontSize: 16
+            ),
+            velocity: 1,
+            space: 150,
+          ),
+          SizedBox(height: 10),
+          FLMarqueeLabel(
+            text: 'Notice: This is a notification',
+            style: TextStyle(
+                color: Colors.lightGreen,
+                fontSize: 16
+            ),
+            loop: false,
+            velocity: 0.7,
+          ),
+          SizedBox(height: 10),
+        ],
+      )
+    );
+  }
+
+  void _showPasteBoardValue() {
+    Clipboard.getData('text/plain').then((ClipboardData data) {
+      setState(() => _clipboardStr = data.text);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
         appBar: AppBar(
-          title: Text('Badge'),
+          title: Text('Labels'),
+          centerTitle: true,
         ),
         body: Container(
-           child: FLMarqueeLabel(
-             padding: EdgeInsets.symmetric(horizontal: 8),
-             velocity: 0,
-             text: 'this is more than a fix',
-           ),
+            width: screenWidth,
+            height: screenHeight,
+           child: ListView(
+             shrinkWrap: true,
+             children: <Widget>[
+               _buildSection('Copyable Label', _buildCopyLabelContent()),
+               _buildSection('Marquee Label', _buildMarqueeLabelContent())
+             ],
+           )
         )
     );
   }
