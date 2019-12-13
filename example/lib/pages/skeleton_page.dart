553d07c235b2e6e56fcc1f3ec72ad95ab141652b
Rannie <18622218653@163.com>
2019-12-13 16:31:44 +0800

    feat: add skeleton demo

--- example/lib/pages/skeleton_page.dart
+++ example/lib/pages/skeleton_page.dart
@@ -9,16 +9,151 @@ class SkeletonPage extends StatefulWidget {
 }
 
 class _SkeletonState extends State<SkeletonPage> {
-  bool _active = true;
-
-  @override
-  void initState() {
-    super.initState();
+  Widget _buildSection(String title, Widget content) {
+    return Column(
+      children: <Widget>[
+        Container(
+          color: Color.fromRGBO(246, 246, 246, 1),
+          padding: EdgeInsets.symmetric(horizontal: 20),
+          height: 56,
+          child: Row(
+            children: <Widget>[
+              Text(title, style: TextStyle(color: Colors.blueGrey, fontSize: 17)),
+            ],
+          ),
+        ),
+        content
+      ],
+    );
   }
 
-  @override
-  void dispose() {
-    super.dispose();
+  Widget _buildNormalContent() {
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: Stack(
+            children: <Widget>[
+              FLSkeleton(
+                shape: BoxShape.circle,
+                margin: EdgeInsets.only(top: 10, left: 10),
+                active: false,
+                width: 40,
+                height: 40,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                margin: EdgeInsets.only(left: 60, top: 10, right: 10),
+                active: false,
+                height: 20,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                margin: EdgeInsets.only(left: 60, top: 40),
+                active: false,
+                width: 300,
+                height: 20,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                margin: EdgeInsets.only(left: 60, top: 70, bottom: 10),
+                active: false,
+                width: 100,
+                height: 20,
+              ),
+            ],
+          ),
+        )
+    );
+  }
+
+  Widget _buildShimmerContent() {
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: Stack(
+            children: <Widget>[
+              FLSkeleton(
+                shape: BoxShape.circle,
+                margin: EdgeInsets.only(top: 10, left: 10),
+                width: 40,
+                height: 40,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                margin: EdgeInsets.only(left: 60, top: 10, right: 10),
+                height: 20,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                margin: EdgeInsets.only(left: 60, top: 40),
+                width: 300,
+                height: 20,
+              ),
+              FLSkeleton(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(2),
+                margin: EdgeInsets.only(left: 60, top: 70, bottom: 10),
+                width: 100,
+                height: 20,
+              ),
+            ],
+          ),
+        )
+    );
+  }
+
+  Widget _buildStretchContent() {
+    return Container(
+        padding: EdgeInsets.all(10),
+        child: Card(
+          child: Container(
+            width: double.infinity,
+            child: Stack(
+              children: <Widget>[
+                FLSkeleton(
+                  shape: BoxShape.circle,
+                  margin: EdgeInsets.only(top: 10, left: 10),
+                  active: false,
+                  width: 40,
+                  height: 40,
+                ),
+                FLSkeleton(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(2),
+                  type: FLSkeletonAnimationType.stretch,
+                  margin: EdgeInsets.only(left: 60, top: 10, right: 10),
+                  height: 20,
+                  width: 320,
+                  stretchWidth: 100,
+                ),
+                FLSkeleton(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(2),
+                  type: FLSkeletonAnimationType.stretch,
+                  margin: EdgeInsets.only(left: 60, top: 40),
+                  width: 300,
+                  stretchWidth: 150,
+                  height: 20,
+                ),
+                FLSkeleton(
+                  shape: BoxShape.rectangle,
+                  borderRadius: BorderRadius.circular(2),
+                  type: FLSkeletonAnimationType.stretch,
+                  margin: EdgeInsets.only(left: 60, top: 70, bottom: 10),
+                  width: 100,
+                  stretchWidth: 200,
+                  height: 20,
+                ),
+              ],
+            ),
+          )
+        )
+    );
   }
 
   @override
@@ -28,46 +163,14 @@ class _SkeletonState extends State<SkeletonPage> {
         title: Text('Skeleton Page'),
       ),
       body: Container(
-        padding: EdgeInsets.all(10),
-        child: Card(
-          child: Stack(
-            children: <Widget>[
-              FLSkeleton(
-                shape: BoxShape.circle,
-                margin: EdgeInsets.only(top: 10, left: 10),
-                active: _active,
-                width: 40,
-                height: 40,
-              ),
-              FLSkeleton(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(2),
-                margin: EdgeInsets.only(left: 60, top: 10, right: 10),
-                active: _active,
-                height: 20,
-              ),
-              FLSkeleton(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(2),
-                type: FLSkeletonAnimationType.stretch,
-                margin: EdgeInsets.only(left: 60, top: 40),
-                width: 300,
-                stretchWidth: 150,
-                height: 20,
-              ),
-              FLSkeleton(
-                shape: BoxShape.rectangle,
-                borderRadius: BorderRadius.circular(2),
-                type: FLSkeletonAnimationType.stretch,
-                margin: EdgeInsets.only(left: 60, top: 70, bottom: 10),
-                width: 100,
-                stretchWidth: 200,
-                height: 20,
-              ),
-            ],
-          ),
-        )
-      ),
+        child: ListView(
+          children: <Widget>[
+            _buildSection('Shimmer', _buildShimmerContent()),
+            _buildSection('Stretch', _buildStretchContent()),
+            _buildSection('No Animation', _buildNormalContent()),
+          ],
+        ),
+      )
     );
   }
 }
