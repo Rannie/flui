dc8eb0c4bf72ce963c808b223e31aa3612f75468
Rannie <18622218653@163.com>
2019-12-16 14:53:11 +0800

    feat: demo home page enhance

---
+++ example/lib/pages/about_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AboutPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => AboutPageState();
+}
+
+class AboutPageState extends State<AboutPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
+  }
+}
