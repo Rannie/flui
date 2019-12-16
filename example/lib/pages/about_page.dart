d5dcf20df8f5140a485001a6f20ae37b43548870
Rannie <18622218653@163.com>
2019-12-16 16:53:01 +0800

    feat: demo complete

--- example/lib/pages/about_page.dart
+++ example/lib/pages/about_page.dart
@@ -1,4 +1,6 @@
+import 'package:example/list_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatefulWidget {
   @override
@@ -6,11 +8,62 @@ class AboutPage extends StatefulWidget {
 }
 
 class AboutPageState extends State<AboutPage> {
+  Widget _headerView() {
+    return Column(
+      children: <Widget>[
+        SizedBox(height: 60),
+        Image.asset('assets/logop.png'),
+        SizedBox(height: 20),
+        Text('version: 0.7.0', style: TextStyle(fontSize: 16, color: Colors.black.withAlpha(160)), textAlign: TextAlign.center),
+        SizedBox(height: 40),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('About'),
+        centerTitle: true,
+      ),
       body: Container(
-
+        child: ListView.separated(
+          itemCount: 4,
+          itemBuilder: (BuildContext context, int index) {
+            if (index == 0) {
+              return _headerView();
+            }
+            else if (index == 1) {
+              return FLListTile(
+                title: Text('Site'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap: () {
+                  const siteUrl = 'https://flui.xin/en/';
+                  canLaunch(siteUrl).then((canOpen) {
+                    launch(siteUrl);
+                  });
+                },
+              );
+            }
+            else if (index == 2) {
+              return FLListTile(
+                title: Text('GitHub'),
+                trailing: Icon(Icons.keyboard_arrow_right),
+                onTap:  () {
+                  const siteUrl = 'https://github.com/Rannie/flui';
+                  canLaunch(siteUrl).then((canOpen) {
+                    launch(siteUrl);
+                  });
+                },
+              );
+            }
+            return null;
+          },
+          separatorBuilder: (context, index) {
+            return Divider(height: 1);
+          }
+        ),
       ),
     );
   }
--- site/docs/en/widgets/index.md
+++ site/docs/en/widgets/index.md
@@ -1,7 +1,5 @@
 # Overview
 
-![overview](http://abtfun.oss-cn-beijing.aliyuncs.com/img/2019-12-15-components_overview.png)
-
 **FLUI** has a relatively fine granularity of widgets, which is a supplement to the already very rich Flutter widgets. There are currently 15 categories and they are still constantly increasing.
 
 Each widget is almost independent (a few have dependencies, such as *FLListTile* to be imported in *FLStaticListView* to get the ability of showing click effect), you can either use `import 'package:flui.dart'` or `import 'package:flui/widgets/avatar.dart'`, tree-shaking will take over the optimization process of dependencies when packaging.
