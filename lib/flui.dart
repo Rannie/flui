82c6ab05d738423fdad0bcb88c9347085fb18fc4
Rannie <18622218653@163.com>
2020-01-22 19:56:54 +0800

    feat: dynamic initial

--- lib/flui.dart
+++ lib/flui.dart
@@ -1,28 +1,6 @@
 library flui;
 
-export 'package:flui/src/common/defines.dart';
-export 'package:flui/src/widgets/flat_button.dart';
-export 'package:flui/src/widgets/raised_button.dart';
-export 'package:flui/src/widgets/gradient_button.dart';
-export 'package:flui/src/widgets/loading_button.dart';
-export 'package:flui/src/widgets/bubble.dart';
-export 'package:flui/src/widgets/bubble_menu.dart';
-export 'package:flui/src/widgets/avatar.dart';
-export 'package:flui/src/widgets/badge.dart';
-export 'package:flui/src/widgets/copyable_label.dart';
-export 'package:flui/src/widgets/toast.dart';
-export 'package:flui/src/widgets/primary_color_override.dart';
-export 'package:flui/src/widgets/pin_input.dart';
-export 'package:flui/src/widgets/auto_complete.dart';
-export 'package:flui/src/widgets/empty.dart';
-export 'package:flui/src/widgets/marquee_label.dart';
-export 'package:flui/src/widgets/notice_bar.dart';
-export 'package:flui/src/widgets/skeleton.dart';
-export 'package:flui/src/widgets/image.dart';
-export 'package:flui/src/widgets/action_sheet.dart';
-export 'package:flui/src/widgets/operation_sheet.dart';
-export 'package:flui/src/widgets/appbar_title.dart';
-export 'package:flui/src/widgets/static_list.dart';
-export 'package:flui/src/widgets/list_tile.dart';
-export 'package:flui/src/widgets/count_stepper.dart';
-export 'package:flui/src/widgets/persistent_header.dart';
+// widgets
+export 'package:flui/widgets.dart';
+// dynamic module
+export 'package:flui/dynamic.dart';
