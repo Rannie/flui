1cea950d315e99648aeddedf54f4fc6e390cf384
Rannie <18622218653@163.com>
2020-03-03 14:54:47 +0800

    fix: notification bar background color issue #19

--- lib/src/widgets/notice_bar.dart
+++ lib/src/widgets/notice_bar.dart
@@ -142,9 +142,9 @@ class FLNoticeBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final bool isDarkMode = Theme.of(context).brightness == Brightness.dark;
     final Color accent = Theme.of(context).accentColor;
-    final Color barBackgroundColor = backgroundColor ?? isDarkMode
+    final Color barBackgroundColor = backgroundColor ?? (isDarkMode
         ? accent.withOpacity(0.2)
-        : FLNoticeBarBackgroundColor;
+        : FLNoticeBarBackgroundColor);
     final Color textColor = isDarkMode ? accent : FLNoticeBarTintColor;
     final TextStyle marqueeStyle = textStyle ?? TextStyle(color: textColor);
 
