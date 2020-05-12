6b596a779411a2c2754dd200eb83dd34be80c453
Rannie <18622218653@163.com>
2020-05-12 21:52:46 +0800

    fix: design language names

--- lib/src/common/tools.dart
+++ lib/src/common/tools.dart
@@ -32,17 +32,27 @@ class FLThemeColors {
 
 class FLThemeTextStyles {
   static const String display4 = 'display4';
+  static const String headline1 = 'headline1';
   static const String display3 = 'display3';
+  static const String headline2 = 'headline2';
   static const String display2 = 'display2';
+  static const String headline3 = 'headline3';
   static const String display1 = 'display1';
+  static const String headline4 = 'headline4';
   static const String headline = 'headline';
+  static const String headline5 = 'headline5';
   static const String title = 'title';
+  static const String headline6 = 'headline6';
   static const String subhead = 'subhead';
+  static const String subtitle1 = 'subtitle1';
   static const String body2 = 'body2';
+  static const String bodyText1 = 'bodyText1';
   static const String body1 = 'body1';
+  static const String bodyText2 = 'bodyText2';
   static const String caption = 'caption';
   static const String button = 'button';
   static const String subtitle = 'subtitle';
+  static const String subtitle2 = 'subtitle2';
   static const String overline = 'overline';
 }
 
@@ -120,29 +130,39 @@ class FLThemeTool {
       TextTheme textTheme = Theme.of(ctx).textTheme;
       switch (descriptor) {
         case FLThemeTextStyles.display4:
-          return textTheme.display4;
+        case FLThemeTextStyles.headline1:
+          return textTheme.headline1;
         case FLThemeTextStyles.display3:
-          return textTheme.display3;
+        case FLThemeTextStyles.headline2:
+          return textTheme.headline2;
         case FLThemeTextStyles.display2:
-          return textTheme.display2;
+        case FLThemeTextStyles.headline3:
+          return textTheme.headline3;
         case FLThemeTextStyles.display1:
-          return textTheme.display1;
+        case FLThemeTextStyles.headline4:
+          return textTheme.headline4;
         case FLThemeTextStyles.headline:
-          return textTheme.headline;
+        case FLThemeTextStyles.headline5:
+          return textTheme.headline5;
         case FLThemeTextStyles.title:
-          return textTheme.title;
+        case FLThemeTextStyles.headline6:
+          return textTheme.headline6;
         case FLThemeTextStyles.subhead:
-          return textTheme.subhead;
+        case FLThemeTextStyles.subtitle1:
+          return textTheme.subtitle1;
         case FLThemeTextStyles.body2:
-          return textTheme.body2;
+        case FLThemeTextStyles.bodyText1:
+          return textTheme.bodyText1;
         case FLThemeTextStyles.body1:
-          return textTheme.body1;
+        case FLThemeTextStyles.bodyText2:
+          return textTheme.bodyText2;
         case FLThemeTextStyles.caption:
           return textTheme.caption;
         case FLThemeTextStyles.button:
           return textTheme.button;
         case FLThemeTextStyles.subtitle:
-          return textTheme.subtitle;
+        case FLThemeTextStyles.subtitle2:
+          return textTheme.subtitle2;
         case FLThemeTextStyles.overline:
           return textTheme.overline;
         default:
